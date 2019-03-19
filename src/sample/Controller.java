830cde1891684ea9d9f7e6ce0ba33952f66c9651
Konrad Gugała <33671971+Szeksick@users.noreply.github.com>
2019-03-19 22:48:36 +0100

    exeptions catched

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,42 +3,44 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
-import java.util.ArrayList;
 
 public class Controller {
     @FXML private TextField inx;
     @FXML private TextField polynominal;
-    @FXML private TableView resultab;
 
     private String polynom;
 
     public void Calculate(ActionEvent e) {
-       float x = Float.parseFloat(inx.getText());
-        polynom = polynominal.getText();
-        //-22x^2+3x^1+6
-        String[] parts = polynom.split("x\\^\\d+\\+?");
-        for (String part : parts) {
-            System.out.print(Float.parseFloat(part)+" ");
+        if (inx.getText() == null || inx.getText().trim().isEmpty() || polynominal.getText() == null || polynominal.getText().trim().isEmpty() ) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Błąd kalkulatora");
+            alert.setHeaderText(null);
+            alert.setContentText("Proszę upewnić się, że pola są uzupełnione");
+            alert.showAndWait();
+        }else {
+            try {
+                float x = Float.parseFloat(inx.getText());
+                polynom = polynominal.getText();
+                //wielomian rozdzielany do tablicy z wyrażenia regularnego
+                String[] parts = polynom.split("x\\^\\d+\\+?");
+                //Obliczenia metodą Hornera
+                float result;
+                result = Float.parseFloat(parts[0]);
+                for (int i = 1; i < parts.length; i++) {
+                    System.out.println(result);
+                    result = result * x + Float.parseFloat(parts[i]);
+                    }
+                }
+            catch (Exception exept) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Błąd kalkulatora");
+                alert.setHeaderText(null);
+                alert.setContentText("Proszę upewnić się, że pola są uzupełnione właściwie");
+                alert.showAndWait();
+            }
         }
-        System.out.println();
-        System.out.print(Float.parseFloat(parts[0])+" ");
-        for(int i=1;i<parts.length+1;i++) {
-            System.out.print(Float.parseFloat(parts[i-1])*x+" ");
-        }
-        System.out.println();
-
-//        //columns
-//        ArrayList<TableColumn> cols = new ArrayList<TableColumn>();
-//        int i = 0;
-//        for (String part : parts) {
-//            cols.add(new TableColumn(toString(i)));
-//            i++;
-//        }
-//        resultab.getColumns().addAll(cols);
-
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Obliczanie wartosci wielomianu metoda hornera");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setTitle("Praca domowa - Konrad Gugała 11A - Wartość wielomianu obliczana metodą Hornera");
+        primaryStage.setScene(new Scene(root, 623, 200));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
     public static void main(String[] args) {
