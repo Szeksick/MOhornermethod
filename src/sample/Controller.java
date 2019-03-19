44c5ed11fd9d98fe6cadadfa059c5d3a5a112b16
Konrad Gugała <33671971+Szeksick@users.noreply.github.com>
2019-03-19 18:38:59 +0100

    input handling

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField inx;
+    @FXML
+    private TextField polynominal;
+    private String polynom = polynominal.getText();
+
+    public void Calculate(ActionEvent e) {
+
+        System.out.println();
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Obliczanie wartosci wielomianu metoda hornera");
-        primaryStage.setScene(new Scene(root, 600, 300));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
